f3d051184d7f5350830352aa774bdef376e0e3f1
SeungHyeonSubb <163542372+SeungHyeonSubb@users.noreply.github.com>
2024-03-20 15:41:20 +0900

    Update 3.py
    for pull request practice

--- 3.py
+++ 3.py
@@ -1,9 +1,9 @@
 Oh Seung Hyeon 202303690 Computer Engineering
-
+Touched by SeungHyeonSubb
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(-2,2,0.001)
-y = 2*x*x
+y = 4*x*x
 plt.plot(x,y)
 plt.title('Quadratic Curve', color='b')
 plt.xlabel('x'+r'$\rightarrow$')
